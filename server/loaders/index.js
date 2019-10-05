904d4c50e36bed76169398ef317dc745ad621c21
Robin Seerig <robin@seerig.de>
2019-10-05 14:49:29 +0200

    remove debug

--- server/loaders/index.js
+++ server/loaders/index.js
@@ -1,8 +1,8 @@
 const consola = require("consola");
 
 // *** Debug
-const QRCode = require("qrcode");
-const PlaylistService = require("../services/playlist.js");
+// const QRCode = require("qrcode");
+// const PlaylistService = require("../services/playlist.js");
 // ***
 
 // import Loaders
@@ -29,17 +29,19 @@ module.exports = {
     consola.success("Nuxt Initialized");
 
     // *** Debug
-    QRCode.toString("www.google.de", {type: "terminal"}, (_err, url) => {
+    /*
+    QRCode.toString("www.google.de", { type: "terminal" }, (_err, url) => {
       console.log(url);
     });
-    
+    */
+
+    /*
     const playlist = new PlaylistService(this.spotifyApi, this.sqlConnection);
-
     const tracks = await playlist.searchTracks("Take on me").catch(e => { console.error(e); });
-
     tracks.forEach(track => {
       playlist.addSong(track.uri);
     });
+    */
     // ***
   }
 };
