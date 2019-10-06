30c3e85d18a538503a67cbf25a47b700a9663550
Moritz Ruth <moritzruthcode@gmail.com>
2019-10-06 10:37:28 +0200

    Set playlist width

--- pages/g/index.vue
+++ pages/g/index.vue
@@ -1,55 +1,60 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-list v-if="tracks.length !== 0" class="first-song" elevation="5">
-        <v-list-item two-line ripple>
-          <v-list-item-icon class="song--icon-wrapper">
-            <v-icon size="2rem">mdi-play</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ tracks[0].title }}</v-list-item-title>
-            <v-list-item-subtitle>{{ tracks[0].artists.join(", ") }}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+    <v-list v-if="tracks.length !== 0" class="track__first track-list" elevation="5">
+      <v-list-item two-line ripple>
+        <v-list-item-icon class="track--icon-wrapper">
+          <v-icon size="2rem">
+            mdi-play
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ tracks[0].title }}</v-list-item-title>
+          <v-list-item-subtitle>{{ tracks[0].artists.join(", ") }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
 
-      <v-list v-if="tracks.length > 1" elevation="2">
-        <v-list-item v-for="track in tracks.slice(1)" :key="track.id" two-line ripple>
-          <v-list-item-content>
-            <v-list-item-title>{{ track.title }}</v-list-item-title>
-            <v-list-item-subtitle>{{ track.artists.join(", ") }}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-flex>
+    <v-list v-if="tracks.length > 1" elevation="2" class="track-list">
+      <v-list-item v-for="track in tracks.slice(1)" :key="track.id" two-line ripple>
+        <v-list-item-content>
+          <v-list-item-title>{{ track.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{ track.artists.join(", ") }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </v-layout>
 </template>
 
 <style scoped lang="scss">
-.first-song {
-  margin-bottom: 20px;
-}
+  .track__first {
+    margin-bottom: 20px;
+  }
 
-.song--icon-wrapper:first-child {
-  margin-right: 10px;
-  margin-bottom: 0;
-}
+  .track--icon-wrapper:first-child {
+    margin-right: 10px;
+    margin-bottom: 0;
+  }
+
+  .track-list {
+    width: 500px;
+    max-width: 100%;
+  }
 </style>
 
 <script>
-export default {
-  data: () => ({
-    headers: [
-      { text: "Title", value: "name" },
-      { text: "Artist", value: "artist" },
-      { text: "Added by", value: "addedBy" }
-    ],
-    tracks: []
-  }),
-  async asyncData({ $axios }) {
-    return {
-      tracks: await $axios.$get("/playlist")
-    };
-  }
-};
+  export default {
+    data: () => ({
+      headers: [
+        { text: "Title", value: "name" },
+        { text: "Artist", value: "artist" },
+        { text: "Added by", value: "addedBy" }
+      ],
+      tracks: []
+    }),
+    async asyncData({ $axios }) {
+      return {
+        tracks: await $axios.$get("/playlist")
+      };
+    }
+  };
 </script>
